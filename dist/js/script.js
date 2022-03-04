971212378b1d7d120eee3124cd0aac5cd40f3234
miguelkapingala <mkcapitango.83@gmail.com>
2022-03-04 20:13:49 -0300

    logica do formulario

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,31 @@
-var name = document.querySelector("#exampleInputName");
+/*var name = document.querySelector("#exampleInputName");
 var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 var birth = document.querySelector("#exampleInputBirth");
 var country = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var file = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var admin = document.querySelector("#exampleInputAdmin"); */
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+var user = {}
+
+fields.forEach(function(field, index) {
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+
+            user[field.name] = field.value;
+        }
+
+    } else {
+
+        user[field.name] = field.value;
+
+    }
+
+});
+console.log(user)
