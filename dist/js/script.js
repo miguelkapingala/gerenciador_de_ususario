5a977626373f5fd86e5982db75fa5b3efba4e5c2
miguelkapingala <mkcapitango.83@gmail.com>
2022-03-04 23:26:08 -0300

    criei models

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,25 +7,47 @@ var password = document.querySelector("#exampleInputPassword");
 var file = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin"); */
 
-
 var fields = document.querySelectorAll("#form-user-create [name]");
 
-var user = {}
+var user = {};
 
-fields.forEach(function(field, index) {
+function addLine(dataUser) {
 
-    if (field.name == "gender") {
+    document.getElementById("table-users").innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>`;
 
-        if (field.checked) {
+}
+
+
+document.getElementById('form-user-create').addEventListener("submit", function() {
+    event.preventDefault();
+
+    fields.forEach(function(field, index) {
+
+        if (field.name == "gender") {
+
+            if (field.checked) {
+
+                user[field.name] = field.value;
+            }
+
+        } else {
 
             user[field.name] = field.value;
+
         }
 
-    } else {
+    });
+    addLine(user);
 
-        user[field.name] = field.value;
-
-    }
-
-});
-console.log(user)
+})
---
+++ models/Users.js
@@ -0,0 +1,18 @@
+class User {
+
+    constructor(name, birth, gender, country, email, password, photo, admin) {
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+
+
+
+    }
+
+
+}
