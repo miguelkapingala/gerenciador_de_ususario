d358e5eb5b40bd89fbc5b992161d1cd7f9ca2411
miguelkapingala <mkcapitango.83@gmail.com>
2022-03-17 13:48:03 -0300

    criar controller

---
+++ controllers/UserController.js
@@ -0,0 +1,139 @@
+class UserController {
+
+    constructor(formId, tableId) {
+
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+        this.onSubmit();
+
+
+    }
+
+    onSubmit() {
+
+        this.formEl.addEventListener("submit", (event) => {
+
+            event.preventDefault();
+
+            let values = this.getValues();
+
+            this.getPhoto().then((content) => {
+
+                values.photo = content;
+
+                this.addLine(values);
+
+            }, (e) => {
+                console.error(e);
+
+
+            });
+
+            this.getPhoto((content) => {
+
+
+            });
+
+
+        });
+
+    }
+
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let filereader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+
+                    return item;
+
+                }
+            });
+
+            //console.log(elements);
+
+            let file = elements[0].files[0]
+
+            filereader.onload = () => {
+
+                resolve(filereader.result);
+
+            };
+
+            filereader.onerror = (e) => {
+                reject(e);
+            }
+            if (file) {
+
+                filereader.readAsDataURL(file);
+
+            } else {
+
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
+
+        })
+
+    }
+
+    getValues() {
+
+        let user = {};
+
+        [...this.formEl.elements].forEach(function(field, index) {
+
+            if (field.name == "gender") {
+
+                if (field.checked) {
+
+                    user[field.name] = field.value;
+                }
+
+            } else {
+
+                user[field.name] = field.value;
+
+            }
+
+        });
+
+        return new User(
+
+            user.name,
+            user.birth,
+            user.gender,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin
+
+        )
+
+    }
+
+
+    addLine(dataUser) {
+
+        this.tableEl.innerHTML = `
+        <tr>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`;
+
+    }
+
+
+}
